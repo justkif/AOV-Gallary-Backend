b41ea83b121d18313c0d1db5866afa6ef2caf796
kif <139427586+justkif@users.noreply.github.com>
2025-05-21 16:59:52 +0700

    [CREATE] Project Init

---
+++ src/main/java/dev/justkif/gallary/GallaryApplication.java
@@ -0,0 +1,13 @@
+package dev.justkif.gallary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallaryApplication.class, args);
+	}
+
+}
