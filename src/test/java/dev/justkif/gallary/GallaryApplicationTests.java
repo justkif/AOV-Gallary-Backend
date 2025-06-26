9d1e5ba03779df60f0b33c9314fea39b98b3de46
kif <139427586+justkif@users.noreply.github.com>
2025-06-26 12:20:07 +0700

    [CREATE] Project Init

---
+++ src/test/java/dev/justkif/gallary/GallaryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.justkif.gallary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
